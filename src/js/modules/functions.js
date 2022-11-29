e49a48cd15804a3d3f7221d871e2da25ea98a1c6
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-11-29 17:08:50 +0400

    tabs

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -23,3 +23,10 @@ $('.banner-section__slider').slick({
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btn-next"> <img src="img/arrow-right.svg" alt="arrow-right"></button>',
 })
 
+$('.search__tabs-item').on('click', function(e) {
+    e.preventDefault();
+    $('.search__tabs-item').removeClass('search__tabs-item--active');
+    $('.search__content-item').removeClass('search__content-item--active');
+    $(this).addClass('search__tabs-item--active');
+    $($(this).attr('href')).addClass('search__content-item--active');
+})
