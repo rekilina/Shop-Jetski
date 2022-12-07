d4228ce6483c64fca5bc59924a89636e137ea17a
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-12-07 16:56:09 +0400

    slider item

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -32,3 +32,26 @@ $('.search__tabs-item').on('click', function(e) {
     $(this).addClass('search__tabs-item--active');
     $($(this).attr('href')).addClass('search__content-item--active');
 })
+
+// работа табов - универсальные классы
+$('.tab').on('click', function(e) {
+    e.preventDefault();
+
+    $('.tab').removeClass('tab--active');
+    $('.tab-content').removeClass('tab-content--active');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tab-content--active');
+})
+
+$('.products-item__favourite').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('products-item__favourite--active');
+})
+
+// кастомные стрелки slick
+$('.products-slider').slick({
+    dots: true,
+    // prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btn-prev"> <img src="img/arrow-left.svg" alt="arrow-left"></button>',
+    // nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btn-next"> <img src="img/arrow-right.svg" alt="arrow-right"></button>',
+})
